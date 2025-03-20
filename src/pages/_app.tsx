7433e2461e24ec4aa8024483effc016a5cf054f3
Lilitha Mdlalana <lilitha.mdlalana@mukuru.com>
2025-03-20 16:09:48 +0200

    Add the primary pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import SidebarWithHeader from "./components/Layout/sidebar/Sidebar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <SidebarWithHeader>
+      <Component {...pageProps} />
+      </SidebarWithHeader>
+    </ChakraProvider>
+  );
 }
